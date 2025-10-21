0009a561a90d9cd2e19e499228cc10d14ac5bf10
Subhashish Nabajja <subhashishnabajja1575643@gmail.com>
2025-10-21 15:50:21 +0530

    chore: Final fixes.

---
+++ src/constants/faqs.ts
@@ -0,0 +1,115 @@
+export const faqs = [
+  {
+    question: "What services does Naivaidya Clinic offer?",
+    answer:
+      "We provide General Medicine Consult, Diabetes Consult, Cardiac Consult, Women's Health Care, Physiotherapy, and Geriatric support. Diagnostics includes Digital X-Ray, Electrocardiogram (ECG), Echocardiography (2D Echo), Sonography, Pathology, Pulmonary Function Test (PFT), IV Infusion Therapy,Vaccinations, Drugless Therapies, and Personalized Nutrition plans all under one roof.",
+  },
+  {
+    question: "Do I need an appointment or can I walk-in?",
+    answer:
+      "Walk-ins are welcome, but we recommend booking an appointment to minimize waiting time and ensure smooth coordination of diagnostic services.",
+  },
+  {
+    question: "What are your clinic timings?",
+    answer:
+      "Our clinic is open from Monday to Saturday from 9:00 am to 8:00 pm. Emergency care may be available on request.",
+  },
+  {
+    question: "What mode of payments are accepted?",
+    answer: "Cash UPI NEFT, we don't accept debit or credit card",
+  },
+  {
+    question: "What diagnostic tests do you provide?",
+    answer:
+      "We offer digital X-rays, ECG, sonography, 2D echo, pathology tests, stress test, and PFT with accurate, high-quality reporting.",
+  },
+  {
+    question: "How long does it take to get my test results?",
+    answer:
+      "ECG, Xray, TMT, PFT, Echo report are provided on the spot. Pathology tests have a variable processing time, basic results are out in 8 working hours.",
+  },
+  {
+    question: "How do I get my report?",
+    answer:
+      "Reports are available in both digital and print form. Digital reports will be sent to your registered mobile number whereas printed reports need to be collected from the centre.",
+  },
+  {
+    question: "What is IV infusion therapy and how does it work?",
+    answer:
+      "It is a customised therapy designed by us, where in the essential medicines are administered directly in the blood stream which means 100% absorption. When taken orally they all pass through your gut and you get only about 50% of their benefits.",
+  },
+  {
+    question: "What all IV therapies do you offer?",
+    answer:
+      "These therapies are tailored made for your needs. So whether it is a medical condition specific drip or a recovery drip after a workout, flight or illness to a wellness drip for glow and anti-aging, we do it all.",
+  },
+  {
+    question: "Is IV therapy safe for everyone?",
+    answer:
+      "IVs are generally safe but require a consultation to assess medical history, allergies, and suitability for specific drips.",
+  },
+  {
+    question: "How long does an IV session take?",
+    answer:
+      "Our IV therapies can take as little as 10min for a mini drip to a 90min session for an advanced drip. Medical conditions require specific time bound administration.",
+  },
+  {
+    question: "Do you provide home visits or teleconsultations?",
+    answer:
+      "Yes, selected consultations can be arranged via telemedicine or at home upon request.",
+  },
+  {
+    question: "Can vaccinations be taken at the clinic?",
+    answer:
+      "Yes. We provide adult and travel vaccinations under medical supervision.",
+  },
+  {
+    question: "Do you provide preventive health checkups?",
+    answer:
+      "Yes, we offer customized health check packages for cardiac, diabetic, and general wellness screening.",
+  },
+  {
+    question: "What are drugless therapies?",
+    answer:
+      "These are natural, non-invasive treatments aimed at healing and pain relief without the use of medications.",
+  },
+  {
+    question:
+      "Do you offer physiotherapy for sports injuries and post-surgery recovery?",
+    answer:
+      "Yes, our physiotherapy programs are designed for injury rehabilitation, post-operative recovery, and improving mobility.",
+  },
+  {
+    question: "What are drugless therapies?",
+    answer:
+      "These are natural, non-invasive treatments aimed at healing and pain relief without the use of medications.",
+  },
+  {
+    question: "What gynecological services do you offer?",
+    answer:
+      "We take care of women during pregnancy and childbirth and in the diagnosis and treatment of diseases of the female reproductive organs. We also specialize in other women's health issues, such as menopause, hormone problems, contraception (birth control), and infertility and also adolescent gynecological issues.",
+  },
+  {
+    question: "Do you offer fertility and pregnancy-related guidance?",
+    answer:
+      "Yes, our gynecology and wellness team offers counseling and diagnostic support for conception planning and pregnancy care.",
+  },
+  {
+    question: "Is my information kept confidential?",
+    answer:
+      "Absolutely. All patient records and discussions are kept private and handled with the highest level of confidentiality.",
+  },
+  {
+    question: "I'm not feeling well, I can't wait till my turn.",
+    answer:
+      "We prioritise emergencies, let our front desk know, we have separate section for emergency cases where you will be attended to immediately.",
+  },
+  {
+    question: "How do I make an appointment?",
+    answer:
+      "You can call us during our working hours or leave us a message through the website about your preferred time and we'll get in touch with you.",
+  },
+] as {
+  question: string;
+  answer: string;
+}[];
